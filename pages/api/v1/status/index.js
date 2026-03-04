611aef84849e9b98aaafa411a062b8feea990d8f
dbq04 <89948676+dbq04@users.noreply.github.com>
2026-03-04 02:34:45 +0000

    add `/api/v1/status` endpoin

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,4 @@
+function status(request, response) {
+  response.status(200).json({ chave: "alunos do curso dev são otimos" });
+}
+export default status;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 function Home() {
   return <h1>Hello world! </h1>;
 }
-function Teste() {
-  console.log("teste");
-}
 export default Home;
